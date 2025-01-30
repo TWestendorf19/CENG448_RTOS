5625f4f5a1645c34dc65ad4b3791fa479d445c23
Tyson Westendorf <137426615+TWestendorf19@users.noreply.github.com>
2025-01-30 15:00:29 -0700

    Added lab 1 code and build

---
+++ Lab_1_code/CMakeFiles/Lab_1_code.elf.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/student/s7460176/CENG448/CENG448_RTOS/Lab_1_code/src/startup_ARMCM3.S" "/home/student/s7460176/CENG448/CENG448_RTOS/Lab_1_code/CMakeFiles/Lab_1_code.elf.dir/src/startup_ARMCM3.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/student/s7460176/CENG448/CENG448_RTOS/Lab_1_code/src/main.c" "CMakeFiles/Lab_1_code.elf.dir/src/main.c.obj" "gcc" "CMakeFiles/Lab_1_code.elf.dir/src/main.c.obj.d"
+  "/home/student/s7460176/CENG448/CENG448_RTOS/Lab_1_code/src/uart.c" "CMakeFiles/Lab_1_code.elf.dir/src/uart.c.obj" "gcc" "CMakeFiles/Lab_1_code.elf.dir/src/uart.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab_1_code/CMakeFiles/Lab_1_code.elf.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "Lab_1_code.bin"
+  "Lab_1_code.hex"
+  "Lab_1_code/bin/Lab_1_code.map"
+  "CMakeFiles/Lab_1_code.elf.dir/src/main.c.obj"
+  "CMakeFiles/Lab_1_code.elf.dir/src/main.c.obj.d"
+  "CMakeFiles/Lab_1_code.elf.dir/src/startup_ARMCM3.S.obj"
+  "CMakeFiles/Lab_1_code.elf.dir/src/uart.c.obj"
+  "CMakeFiles/Lab_1_code.elf.dir/src/uart.c.obj.d"
+  "bin/Lab_1_code.elf"
+  "bin/Lab_1_code.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/Lab_1_code.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
